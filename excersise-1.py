cc66e1bef5c2556d8adc68b9a3f21ee71272d4c1
Oscar Wellner <oscar.wellner@gmail.com>
2021-09-05 13:32:42 +0200

    Refactoring

--- excersise-1.py
+++ excersise-1.py
@@ -3,37 +3,35 @@
 # About:    This Python script will ask the User his name, age, and month of birth and returns his name, age and the year
 #           when the User will reach a specific age.
 
-import datetime
+from datetime import datetime
 
-MONTHS = ['January', 'Februari', 'March', 'April', 'May', 'June',
+MONTHS = ['January', 'February', 'March', 'April', 'May', 'June',
           'July', 'August', 'September', 'Oktober', 'November', 'December']
 AGE_TO_CALCULATE = 100
 
-# Returns a int value from the inputted value, if possible.
 
-
-def get_int_from_input(message):
+def get_age(message):
+    # Returns the age value from the inputted value, if possible.
     value = input(message)
 
-    if value.isdigit:
-        value = int(value)
-    else:
+    if not value.isdigit:
         print(
             f"The inputted value: '{value}' is not a digit. Please try again...")
 
-        return get_int_from_input(message)
+        return get_age(message)
+
+    value = int(value)
 
     if value <= 0:
         print(f"No person can be of age: '{value}'. Please, try again...")
 
-        return get_int_from_input(message)
+        return get_age(message)
 
     return value
 
-# Returns the numeric value of the inputted month ( 0 - 11 ).
-
 
 def get_month():
+    # Returns the numeric value of the inputted month ( 0 - 11 ).
     month = input('In what month is your birthday? ')
 
     if month.capitalize() not in MONTHS:
@@ -45,22 +43,22 @@ def get_month():
     return MONTHS.index(month.capitalize())
 
 
-# Returns the year in which the age to calculate is reached depending on the provided age.
 def calc_year(age, birthday_month):
-    current_date = datetime.datetime.now()
+    # Returns the year in which the age to calculate is reached depending on the provided age.
+    current_date = datetime.now()
 
     years_to_go = AGE_TO_CALCULATE - age
 
     if current_date.month < birthday_month:
         years_to_go -= 1
 
-    return datetime.datetime(current_date.year + years_to_go,
-                             current_date.month, current_date.day).year
+    return datetime(current_date.year + years_to_go,
+                    current_date.month, current_date.day).year
 
 
 def main():
     name = input('What is your name? ')
-    age = get_int_from_input('How old are you? ')
+    age = get_age('How old are you? ')
     month_of_birth_int = get_month()
     calculated_year = calc_year(age, month_of_birth_int)
 
